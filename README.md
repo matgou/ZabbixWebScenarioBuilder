462271ba4130ccef983bae5209bd1ca0d345bd46
Mathieu GOULIN <mathieu.goulin@gadz.org>
2020-11-04 23:51:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+venv/bin/mitmdump -p 3128 -w output
+python main.py output SUPERVISION_SITE_PHOTOS
---
+++ main.py
@@ -0,0 +1,71 @@
+import json, sys
+from mitmproxy import io
+from mitmproxy.exceptions import FlowReadException
+from pyzabbix import ZabbixAPI
+import configparser
+import logging
+import logging.config
+
+if __name__ == '__main__':
+    logging.config.fileConfig('logging.conf')
+    logging.info('Demarage du programme de conversion mitmproxy2zabbix')
+    mitmproxy_dump=sys.argv[1]
+    scenario_name=sys.argv[2]
+
+    logging.debug('Chargement de la configuration depuis config.ini')
+    config=configparser.ConfigParser()
+    config.read('config.ini')
+
+    zabbix_host=config['DEFAULT']['zabbix_host']
+    zabbix_user=config['DEFAULT']['zabbix_user']
+    zabbix_password=config['DEFAULT']['zabbix_password']
+    zabbix_hostid=config['DEFAULT']['zabbix_hostid']
+    zabbix_ignore_ext=config['DEFAULT']['zabbix_ignore_ext'].split(',')
+
+    # Create ZabbixAPI class instance
+    logging.info('Connexion au serveur {}'.format(zabbix_host))
+    zapi = ZabbixAPI(server=zabbix_host)
+    zapi.login(zabbix_user, zabbix_password)
+
+    logging.info('Read mitmproxy dump file : {}'.format(mitmproxy_dump))
+    with open(mitmproxy_dump, "rb") as logfile:
+        freader = io.FlowReader(logfile)
+        try:
+            i=1
+            steps=[]
+            for f in freader.stream():
+                ignore_step=False
+                request = f.request
+                for ext in zabbix_ignore_ext:
+                    if request.path.endswith(ext):
+                        ignore_step=True
+                response = f.response
+                logging.debug('{} {}/{}'.format(request.method, request.host, request.path))
+                step={
+                  'name': 'step {}'.format(i),
+                  'url': '{}://{}{}'.format(request.scheme, request.host, request.path),
+                  'no': i,
+                }
+                if request.method == 'POST':
+                    post_data=[]
+                    for d in request.text.split('&'):
+                        if d != '':
+                            p=d.split('=')
+                            field={'name': p[0], 'value': p[1]}
+                            post_data.append(field)
+                    step['posts']=post_data
+
+                #            curl = 'curl -X ' + request.method + ' -d \'' + request.content + '\' ' + ' '.join(['-H ' + '"' + header[0] + ': ' + header[1] + '"' for header in request.headers])
+#                curl += " https://" + request.host + request.path
+#                print(curl)
+#            pri    nt("--")
+                if ignore_step == False:
+                    logging.info('Step {} : {}'.format(i,json.dumps(step)))
+                    steps.append(step)
+                i=i+1
+        except FlowReadException as v:
+            print("Flow file corrupted. Stopped loading.")
+
+    logging.info('Create Webscenario in zabbix')
+    zapi.do_request('httptest.create',{'name': scenario_name, 'hostid': zabbix_hostid, 'steps': steps})
+    logging.info('End of bash')
